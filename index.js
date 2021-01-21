4f2487741821ce1d6f5fc840eaca580fa9ca6916
Caleb Glorvigen <jcglorvi@gmail.com>
2021-01-20 20:28:46 -0500

    Created the init function with an inquirer prompt and it's promise

--- index.js
+++ index.js
@@ -15,11 +15,50 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    // init();
-        console.log("string connect to the database");
+    init();
+    // console.log("string connect to the database");
 
 });
 
-function init(){
+function init() {
+    inquirer
+        .prompt({
+            name: "selection",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles",
+                "Add Role", "View All Departments", "Add Department"
+            ]
+        })
+        .then(function (answer) {
 
+            if (answer.selection === "View All Employees") {
+                viewAllEmployees();
+                //             } else if (answer.selection === "View All Employees By Department") {
+                //                  //last
+                //             } else if (answer.selection === "View All Employees By Manager") {
+                //                  //last
+            } else if (answer.selection === "Add Employee") {
+                addEmployee();
+                // } else if (answer.selection === "Remove Employee") {
+                //     //last
+            } else if (answer.selection === "Update Employee Role") {
+                updateEmployeeRole();
+                // } else if (answer.selection === "Update Employee Manager") {
+                //      //last
+            } else if (answer.selection === "View All Roles") {
+                viewAllRoles();
+            } else if (answer.selection === "Add Role") {
+                addRole();
+                // } else if (answer.selection === "Remove Role") {
+                //     //last
+            } else if (answer.selection === "View All Departments") {
+                viewAllDepartments();
+            } else if (answer.selection === "Add Department") {
+                addDepartment();
+            } else {
+                connection.end();
+            }
+        });
 }
+
