e2ac23dad4c8a77a38a1aff0a5cdacb81423426a
Caleb Glorvigen <jcglorvi@gmail.com>
2021-01-21 20:17:14 -0500

    Updated the seedDB to include some rows of data.

--- db/seedDB.sql
+++ db/seedDB.sql
@@ -12,8 +12,9 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
-    salary DECIMAL(10,4) NULL,
-    -- department_id 
+    salary DECIMAL(10,3) NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department (id),
     PRIMARY KEY (id)
 );
 
@@ -21,8 +22,23 @@ CREATE TABLE employee(
     id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    -- role_id
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id),
     PRIMARY KEY(id)
 );
 
-SELECT * FROM department, role, employee;
+INSERT INTO department (name)
+VALUES("Engineering"), ("Finance");
+
+INSERT INTO role (title, salary, department_id)
+VALUES("Software Engineer", 50000, 1), ("Accountant", 125000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES("Mike", "Lourd", 1), ("Kevin", "Tupik", 2);
+
+SELECT * FROM role, employee, department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
